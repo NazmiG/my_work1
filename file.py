ae0396e8ab5d5d4a419cc68988d8deb9e5750821
NazmiG <gendzhnazmi@gmail.com>
2018-11-20 16:03:41 +0000

    updated

--- file.py
+++ file.py
@@ -10,3 +10,34 @@ a=((b**2 + c**2)**(1/2));
 print(a)
 print(type(a))
 
+
+mv file.py my_work1
+
+it config --global user.name "Your Name"
+    git config --global user.email you@example.com
+
+If the identity used for this commit is wrong, you can fix it with:
+
+    git commit --amend --author='Your Name <you@example.com>'
+
+git config --global user.name "NazmiG"
+
+
+git config --global user.email gendzhnazmi@gmail.com
+
+
+git add   .
+
+!!! git push
+
+ git commit --amend --author='NazmiG  <gendzhnazmi@gmail.com>'
+
+!!! git commit -m 'Comment about Y u added file'
+
+
+
+gedit file.py &
+
+
+
+
