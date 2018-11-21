ec905feee2dba5c89bd768fa9f33157c253104b8
NazmiG <gendzhnazmi@gmail.com>
2018-11-21 14:09:24 +0000

    updates to file.py

--- file.py
+++ file.py
@@ -1,43 +1,6 @@
 
 
-# course=('python');
-# rating=0;
-# print(course,rating)
-
-b=3;
-c=4;
-a=((b**2 + c**2)**(1/2));
-print(a)
-print(type(a))
-
-
-mv file.py my_work1
-
-it config --global user.name "Your Name"
-    git config --global user.email you@example.com
-
-If the identity used for this commit is wrong, you can fix it with:
-
-    git commit --amend --author='Your Name <you@example.com>'
-
-git config --global user.name "NazmiG"
-
-
-git config --global user.email gendzhnazmi@gmail.com
-
-
-git add   .
-
-!!! git push
-
- git commit --amend --author='NazmiG  <gendzhnazmi@gmail.com>'
-
-!!! git commit -m 'Comment about Y u added file'
-
-
-
-gedit file.py &
-
-
-
+with open("weather.csv","r") as a reader:
+	data=reader.readline()
+print(data)
 
